22c9a62c91829999b33814bf4218caac325695bd
Your Name <6x241rn@code.edu.az>
2023-04-18 11:57:16 +0400

    18.04

---
+++ main.js
@@ -0,0 +1,121 @@
+//TASK!//
+let calculationAnonim=function(num){
+    if(num%7==0){
+        console.log("Eded 7 ye bolunur")
+    }
+    else {
+        for (let i = 0; i < num%7==0; i++) {
+           
+               
+               result= i-num > i-num;
+            
+            
+      
+            
+        }
+        console.log(result)
+    }
+}
+calculationAnonim(20)
+
+
+
+//TASK2//
+// function calculation(num) {
+//     if (num <50) {
+//         for (let i = 0; i < num; i++) {
+            
+//             if(i%3==0){
+//                 console.log(i);
+//             }
+//         }
+//     }
+//     else if(num>50 && num<100){
+//         for (let i = 0; i < num; i++) {
+            
+//             if(i%5==0){
+//                 console.log(i);
+//             }
+//         }
+//     }
+//    else{
+//     for (let i = 0; i < num; i++) {
+            
+//         if(i%8==0){
+//             console.log(i);
+//         }
+//     }
+//    }
+    
+// }
+
+// calculation(446)
+
+
+//TASK3//
+
+// let arr=["", "yanvar","fevral","mart","aprel", "may","iyun","iyul","avust","sentyabr","oktyabr","noyabr",'dekabr']
+// let calculationArrow = (num)=>{
+// for (let i = 0; i < arr.length; i++) {
+//     if(num==i){
+//         console.log(arr[i])
+//     }
+// }
+// if(num>12 || num<0){
+//     console.log("invalid input")
+// }
+// }
+// calculationArrow(4)
+
+
+//TASK4//
+
+// let calculationAnonim=function (num,a) {
+//     cem=" ";
+// for (let index = 0; index < num; index++) {
+//     cem=cem+=a;
+    
+// }
+// console.log(cem)
+// }
+// calculationAnonim(23,"a")
+
+
+//TASK5//
+
+
+// function calculation(num){
+//   a=0;
+//     for (let i = 0; i < num; i++) {
+
+//         if(num%i==0){
+//            a+=i;
+           
+//         }
+        
+//     }
+    
+//     if(a==num){
+//         console.log("Perfectdir")
+//     }
+//     else{
+//         console.log("Perfect deyill")
+//     }
+// }
+// calculation(6)
+
+
+
+//TASK6//
+
+// function calculation(num){
+//   result=1;
+//     for (let i = 1; i <= num; i++) {
+
+//           result*=i;
+//     }
+//     console.log(result)
+// }
+// calculation(5)
+
+
